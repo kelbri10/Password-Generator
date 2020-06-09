ba02f40e851e699d0eaa273ea2a9b6b6fe25c9d1
kelbri10 <kelbri10@gmail.com>
2020-06-09 13:06:50 -0400

    set conditions for if-else statements for password criteria

--- script.js
+++ script.js
@@ -20,27 +20,58 @@ function generate(char, num, specialChar, capLetter){
     //so num + special + cap + lower = char
 
     //places alphabet into an array
-    var alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    var int = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    var symbol = 
+    var alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; //26
+    var int = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] //10
+    var symbol = ['!','@','#','$','%','^','&','*','(',')','-','_','+','=','{','}']; //16
 
     var password = []; 
+
     //take the number of characters, sets the limit for the password generator
     for (var i=0; i < char; i++){
-        var random = Math.floor(Math.random() * 26);
+        //var random = Math.floor(Math.random() * 26);
 
         //pulls from 3 arrays - num, special characters, capital Letters
         if ((num == true)&&(specialChar == true)&&(capLetter == true)){
 
+            //combines alpha, int, symbol arrays 
+            var passwordArray = alpha.concat(int, symbol); 
+
+            //randomly generates number based on combined lengths 
+            var random = Math.floor(Math.random() * (int.length + symbol.length + alpha.length));
+
+            //element in password are taken randomly from passwordArray to generate user password 
+            password[i] = passwordArray[random]; 
         }
         //pull from numbers, special characters
         else if ((num == true)&& (specialChar == true)){
+            var passwordArray= alpha.concat(int, symbol); 
+            var random = Math.floor(Math.random() * (int.length + symbol.length + alpha.length)); 
 
+            password[i] = passwordArray[random]; 
         }
         //pull from numbers, alpha
-        else if ((num == true)){
+        else if ((num == true)&&(capLetter == true)){
+            var passwordArray = alpha.concat(int); 
+            var random = Math.floor(Math.random() * (int.length + alpha.length)); 
 
+            //generates user password 
+            password[i] = passwordArray[random]; 
         }
+
+        else if ((specialChar == true)&& capLetter == true){ 
+            var passwordArray = alpha.concat(symbol); 
+            var random = Math.floor(Math.random() * (symbol.length + alpha.length)); 
+
+        
+            password[i] = passwordArray[random]; 
+        }
+
+        else if (capLetter == true){ 
+           var random = Math.floor(Math.random() * alpha.length);
+
+           password[i] = upperCase[random]; 
+        }
+
         //pull from alpha only
         else{ 
             password[i] = alpha[random];
@@ -48,5 +79,6 @@ function generate(char, num, specialChar, capLetter){
     } 
 
     //joins array into string 
-    console.log(password.join('')); 
+    console.log(passwordArray); 
+    console.log(password.join(''), ', num = ' + num, 'specialChar = ' + specialChar, 'capLetter = ' + capLetter); 
 }
